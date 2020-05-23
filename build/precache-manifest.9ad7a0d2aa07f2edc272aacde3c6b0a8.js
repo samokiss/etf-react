41141e7232147cd02680b25199411deba75a8bb3
samokiss <gomis.samuel@gmail.com>
2020-05-23 12:51:59 +0200

    add build folder

---
+++ build/precache-manifest.9ad7a0d2aa07f2edc272aacde3c6b0a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c6e51359bb1aae9dc2c",
+    "url": "/static/css/main.708d469a.chunk.css"
+  },
+  {
+    "revision": "3c6e51359bb1aae9dc2c",
+    "url": "/static/js/main.3c6e5135.chunk.js"
+  },
+  {
+    "revision": "4e3f3ef80443dc6c676d",
+    "url": "/static/js/1.4e3f3ef8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1c7fe315329346acbef8ddccb7f728d7",
+    "url": "/index.html"
+  }
+];
