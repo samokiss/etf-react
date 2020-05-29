94112240e6ec8ab675f17419dce594c88e3110b4
samokiss <gomis.samuel@gmail.com>
2020-05-29 17:12:52 +0200

    push build for production

---
+++ build/precache-manifest.a0adbdf4731090ae0660d5a1010a809a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4af69546826a4e11cd1",
+    "url": "/static/css/main.01d43b4d.chunk.css"
+  },
+  {
+    "revision": "a4af69546826a4e11cd1",
+    "url": "/static/js/main.a4af6954.chunk.js"
+  },
+  {
+    "revision": "f8a907d213659c216967",
+    "url": "/static/js/1.f8a907d2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76e8415797091850b3f4dbea0ab6180c",
+    "url": "/index.html"
+  }
+];
