5d8b2fec0b40612d875e51b31db855716df88b6d
samokiss <gomis.samuel@gmail.com>
2022-01-21 13:45:48 +0100

    app template

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = { apiKey: "AIzaSyCJ13PmveNQNbDDSbuWNY-lVRCjDwE29zo",
-    authDomain: "exprime-ta-foi.firebaseapp.com",
-    databaseURL: "https://exprime-ta-foi.firebaseio.com",
-    projectId: "exprime-ta-foi",
-    storageBucket: "exprime-ta-foi.appspot.com",
-    messagingSenderId: "86712728661",
-    appId: "1:86712728661:web:a139e49b96f1cdde8a28dc",
-    measurementId: "G-FENCMVEZ5K"};
